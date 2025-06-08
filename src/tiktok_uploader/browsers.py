e7b9712542f0f65659f8e2eead780c6274440694
Sanjxz <36453187+HyperRamzey@users.noreply.github.com>
2025-06-08 06:02:29 +0300

    Update browsers.py

--- src/tiktok_uploader/browsers.py
+++ src/tiktok_uploader/browsers.py
@@ -1,18 +1,7 @@
 """Gets the browser's given the user's input"""
 from selenium.webdriver.chrome.options import Options as ChromeOptions
-from selenium.webdriver.firefox.options import Options as FirefoxOptions
-from selenium.webdriver.safari.options import Options as SafariOptions
-from selenium.webdriver.edge.options import Options as EdgeOptions
 
-# Webdriver managers
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.firefox.service import Service as FirefoxService
-from webdriver_manager.firefox import GeckoDriverManager
-from webdriver_manager.microsoft import EdgeChromiumDriverManager
-from selenium.webdriver.edge.service import Service as EdgeService
-
-from selenium import webdriver
+import undetected_chromedriver as uc
 
 from tiktok_uploader import config
 from tiktok_uploader.proxy_auth_extension.proxy_auth_extension import (
@@ -20,67 +9,37 @@ from tiktok_uploader.proxy_auth_extension.proxy_auth_extension import (
 )
 
 
-def get_browser(name: str = "chrome", options=None, *args, **kwargs) -> webdriver:
+def get_browser(name: str = "chrome", options: ChromeOptions = None, **kwargs) -> uc.Chrome:
     """
     Gets a browser based on the name with the ability to pass in additional arguments
     """
+    if _clean_name(name) != "chrome":
+        raise UnsupportedBrowserException(
+            f"Unsupported browser: {name}. Only Chrome is supported."
+        )
 
-    # get the web driver for the browser
-    driver_to_use = get_driver(name=name, *args, **kwargs)
+    browser_options = options or get_default_options(name=name, **kwargs)
 
-    # gets the options for the browser
-
-    options = options or get_default_options(name=name, *args, **kwargs)
-
-    # combines them together into a completed driver
-    service = get_service(name=name)
-    if service:
-        driver = driver_to_use(service=service, options=options)
-    else:
-        driver = driver_to_use(options=options)
+    driver = uc.Chrome(options=browser_options, use_subprocess=True, **kwargs)
 
     driver.implicitly_wait(config["implicit_wait"])
 
     return driver
 
 
-def get_driver(name: str = "chrome", *args, **kwargs) -> webdriver:
-    """
-    Gets the web driver function for the browser
-    """
-    if _clean_name(name) in drivers:
-        return drivers[name]
-
-    raise UnsupportedBrowserException()
-
-
-def get_service(name: str = "chrome"):
-    """
-    Gets a service to install the browser driver per webdriver-manager docs
-
-    https://pypi.org/project/webdriver-manager/
-    """
-    if _clean_name(name) in services:
-        return services[name]()
-
-    return None  # Safari doesn't need a service
-
-
-def get_default_options(name: str, *args, **kwargs):
+def get_default_options(name: str, **kwargs) -> ChromeOptions:
     """
     Gets the default options for each browser to help remain undetected
     """
     name = _clean_name(name)
 
-    if name in defaults:
-        return defaults[name](*args, **kwargs)
+    if name == "chrome":
+        return chrome_defaults(**kwargs)
 
     raise UnsupportedBrowserException()
 
 
-def chrome_defaults(
-    *args, headless: bool = False, proxy: dict = None, **kwargs
-) -> ChromeOptions:
+def chrome_defaults(headless: bool = False, proxy: dict = None, **kwargs) -> ChromeOptions:
     """
     Creates Chrome with Options
     """
@@ -119,58 +78,6 @@ def chrome_defaults(
     return options
 
 
-def firefox_defaults(
-    *args, headless: bool = False, proxy: dict = None, **kwargs
-) -> FirefoxOptions:
-    """
-    Creates Firefox with default options
-    """
-
-    options = FirefoxOptions()
-
-    # default options
-
-    if headless:
-        options.add_argument("--headless")
-    if proxy:
-        raise NotImplementedError("Proxy support is not implemented for this browser")
-    return options
-
-
-def safari_defaults(
-    *args, headless: bool = False, proxy: dict = None, **kwargs
-) -> SafariOptions:
-    """
-    Creates Safari with default options
-    """
-    options = SafariOptions()
-
-    # default options
-
-    if headless:
-        options.add_argument("--headless")
-    if proxy:
-        raise NotImplementedError("Proxy support is not implemented for this browser")
-    return options
-
-
-def edge_defaults(
-    *args, headless: bool = False, proxy: dict = None, **kwargs
-) -> EdgeOptions:
-    """
-    Creates Edge with default options
-    """
-    options = EdgeOptions()
-
-    # default options
-
-    if headless:
-        options.add_argument("--headless")
-    if proxy:
-        raise NotImplementedError("Proxy support is not implemented for this browser")
-    return options
-
-
 # Misc
 class UnsupportedBrowserException(Exception):
     """
@@ -178,9 +85,6 @@ class UnsupportedBrowserException(Exception):
 
     Supported browsers are:
         - Chrome
-        - Firefox
-        - Safari
-        - Edge
     """
 
     def __init__(self, message=None):
@@ -192,25 +96,3 @@ def _clean_name(name: str) -> str:
     Cleans the name of the browser to make it easier to use
     """
     return name.strip().lower()
-
-
-drivers = {
-    "chrome": webdriver.Chrome,
-    "firefox": webdriver.Firefox,
-    "safari": webdriver.Safari,
-    "edge": webdriver.ChromiumEdge,
-}
-
-defaults = {
-    "chrome": chrome_defaults,
-    "firefox": firefox_defaults,
-    "safari": safari_defaults,
-    "edge": edge_defaults,
-}
-
-
-services = {
-    "chrome": lambda: ChromeService(ChromeDriverManager().install()),
-    "firefox": lambda: FirefoxService(GeckoDriverManager().install()),
-    "edge": lambda: EdgeService(EdgeChromiumDriverManager().install()),
-}
